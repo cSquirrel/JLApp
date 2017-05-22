ea240512b781fab568a6ff26f49b881c34c9bf63
Marcin Maciukiewicz <ciukes@Marcins-MacBook-Pro.local>
2017-05-22 17:48:09 +0100

    ProductGridAPI: Created model object to encapsulate data returned by the grid api call

--- DishwashersApp/DishwashersApp/API/JohnLewisAPI.swift
+++ DishwashersApp/DishwashersApp/API/JohnLewisAPI.swift
@@ -8,9 +8,36 @@
 
 import UIKit
 
+struct JohnLewisProduct {
+    
+    // This is the product id that should be used to retrieve the product 
+    // information for the product details screen.
+    let productId: String
+    
+    // We can assume for this test that the price is in £s.
+    let price: String
+    
+    // This is the title that should be used on the product grid page
+    let title: String
+    
+    // The url of the image to show on the grid page.
+    let imageURL: URL
+}
+
 class JohnLewisAPI: NSObject {
 
-    func getProductsGrid() -> [Any] {
-        return []
+    func getProductsGrid() -> [JohnLewisProduct] {
+        return [JohnLewisProduct(productId:"product_1",
+                                 price: "9.99",
+                                 title: "Product #1",
+                                 imageURL: URL(string: "http://test.server.com/image1.png")!),
+                JohnLewisProduct(productId:"product_2",
+                                 price: "9.99",
+                                 title: "Product #3",
+                                 imageURL: URL(string: "http://test.server.com/image2.png")!),
+            JohnLewisProduct(productId:"product_3",
+                             price: "9.99",
+                             title: "Product #3",
+                             imageURL: URL(string: "http://test.server.com/image3.png")!)]
     }
 }
