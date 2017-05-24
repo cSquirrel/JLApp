9a238619d5e9c24ae884b5b2e13ecfbf6e9a47f9
Marcin Maciukiewicz <ciukes@Marcins-MacBook-Pro.local>
2017-05-24 08:24:35 +0100

    Product Page UI: Added details to the product page

---
+++ DishwashersApp/DishwashersApp/UI/UI Components/ProductPhotosCarousel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductPhotosCarousel.swift
+//  DishwashersApp
+//
+//  Created by Marcin Maciukiewicz on 24/05/2017.
+//  Copyright © 2017 Blue Pocket Limited. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ProductPhotosCarousel: DesignableView {
+
+}
